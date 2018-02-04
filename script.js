e88f6120b4c6410dd3c286c572bbf0f41893d65e
wchuanghard <wchuang@g.harvard.edu>
2018-02-04 14:19:00 -0800

    updates

---
+++ script.js
@@ -0,0 +1,36 @@
+import Typed from 'typed.js';
+
+$(document).ready(function(){
+   	// Activate Carousel
+   $("#myCarousel").carousel();
+   	// Enable Carousel Indicators
+   	$(".item1").click(function(){
+   	$("#myCarousel").carousel(0);
+   });
+	
+  $(".item2").click(function(){
+   	$("#myCarousel").carousel(1);
+   });
+	
+   $(".item3").click(function(){
+   	$("#myCarousel").carousel(2);
+   });
+	
+   $(".item4").click(function(){
+   	$("#myCarousel").carousel(3);
+   });
+	
+   // Enable Carousel Controls
+   $(".left").click(function(){
+  	$("#myCarousel").carousel("prev");
+   });
+	
+   $(".right").click(function(){
+   	$("#myCarousel").carousel("next");
+   });
+});
+		
+//javascript for typed.js!!
+var typed = new Typed('.element', {
+	stringsElement: '#typed-strings'
+});
